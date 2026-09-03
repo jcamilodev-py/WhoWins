3d8eaca165ce1b5c7abe096702e0f0d43da8b0a2
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-09-03 16:03:12 -0500

    feat(auth): schemas for authentication

---
+++ app/auth/schemas.py
@@ -0,0 +1,49 @@
+import uuid
+
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+from pydantic.alias_generators import to_camel
+
+from app.user.models import Role
+
+
+class RegisterRequest(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+    email: EmailStr = Field(..., min_length=8, description="Email is required")
+    password: str = Field(..., min_length=8, description="Password is required")
+    role: Role = Field(..., description="Role is required")
+
+class ForgotPasswordRequest(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+    email: EmailStr = Field(..., min_length=1, description="email is required")
+
+
+class ResetPasswordRequest(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+    token: str = Field(..., min_length=1, description="token is required")
+    new_password: str = Field(..., min_length=8, description="new password is required")
+
+
+class ChangePasswordRequest(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+    current_password: str = Field(..., min_length=1, description="current password is required")
+    new_password: str = Field(..., min_length=8, description="new password is required")
+
+
+class UserPublic(BaseModel):
+    model_config = ConfigDict(from_attributes=True, alias_generator=to_camel, populate_by_name=True)
+
+    id: uuid.UUID
+    email: EmailStr
+    role: Role
+
+
+class AuthResponse(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+    access_token: str
+    token_type: str = "bearer"
+    user: UserPublic
