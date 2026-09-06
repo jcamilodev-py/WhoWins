d7678909265857d29b80ce99ee8ce147c4908a84
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-09-06 13:49:10 -0500

    feat(main): run uvicorn server from entrypoint

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def main():
-    print("Hello from whowins!")
+import uvicorn
+
+from app.core.settings import settings
+
+
+def main() -> None:
+    uvicorn.run(
+        "app.main:app",
+        host=settings.server_host,
+        port=settings.server_port,
+        reload=settings.is_dev,
+    )
 
 
 if __name__ == "__main__":
