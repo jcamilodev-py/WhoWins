236bebcc531266c3507893ec065893018af68a85
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-09-06 13:48:28 -0500

    feat(core): make rate limit storage configurable

--- app/core/limiter.py
+++ app/core/limiter.py
@@ -1,4 +1,6 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_address)
+from app.core.settings import settings
+
+limiter = Limiter(key_func=get_remote_address, storage_uri=settings.rate_limit_storage_uri)
